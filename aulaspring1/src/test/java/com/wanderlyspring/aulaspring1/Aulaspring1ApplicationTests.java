6d62d15c3dc3444e55f85a9ca809b48c9df3a31a
Wanderly Robson da Silva <wanderly@itexto.com.br>
2023-02-06 13:19:19 -0300

    Estudo SpringBoot

---
+++ aulaspring1/src/test/java/com/wanderlyspring/aulaspring1/Aulaspring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderlyspring.aulaspring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aulaspring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
